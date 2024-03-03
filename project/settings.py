1b0832eb33e55a05f8db5f93f716980903c016fb
Hani <h.informatique7@gmail.com>
2024-03-03 16:25:15 +0100

    dash

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
+LOGIN_REDIRET_URL = 'index'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
